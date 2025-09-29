4026b27597276009e03e0d0bd3e8df93a0311153
DarkShadowBlood <63405827+DarkShadowBlood@users.noreply.github.com>
2025-09-29 13:01:10 -0400

    Delete site web directory

--- site web/app web/cancel_action.ps1
+++
@@ -1,3 +0,0 @@
-# cancel_action.ps1
-# Crée un fichier pour signaler l'annulation
-New-Item -Path "C:\important\ApexWear\app web pour apex\app web - Monde Vivant\site web\app web\cancel_flag.txt" -ItemType File -Force
--- site web/app web/launch_batch.ps1
+++
@@ -1,3 +0,0 @@
-# launch_batch.ps1
-$batchPath = "C:\important\ApexWear\app web pour apex\app web - Monde Vivant\site web\app web\lancer_site.bat"
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$batchPath`"" -Verb RunAs
